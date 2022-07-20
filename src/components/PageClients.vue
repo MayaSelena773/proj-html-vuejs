1894ef1b2a2b45e0d94241cb9f55c84b96abb3e6
MayaSelena773 <maya.bono721@gmail.com>
2022-07-20 14:02:42 +0200

    team

--- src/components/PageClients.vue
+++ src/components/PageClients.vue
@@ -16,17 +16,21 @@
             </div>
         </div>
 
-        <!--Seconda parte-->
+        <!--Seconda parte (relationship)-->
         <div class="flex padd-section">
             <div class="big-box padd-text">
-                <p class="large-text">We Build Relationships</p>
-                <p>Curabitur ac leo nunc. Vestibulum et mauris vel ante finibus maximus nec ut leo. Integer consectetur.</p>
-                <p class="large-text">Propietary Processes</p>
-                <p>Curabitur ac leo nunc. Vestibulum et mauris vel ante finibus maximus nec ut leo. Integer consectetur.</p>
+                <div class="m-b">
+                    <p class="large-text">We Build Relationships</p>
+                    <p>Curabitur ac leo nunc. Vestibulum et mauris vel ante finibus maximus nec ut leo. Integer consectetur.</p>
+                </div>
+                <div class="m-b">
+                    <p class="large-text">Propietary Processes</p>
+                    <p>Curabitur ac leo nunc. Vestibulum et mauris vel ante finibus maximus nec ut leo. Integer consectetur.</p>
+                </div>
             </div>
 
             <div class="big-box">
-                <div class="flex">
+                <div class="flex m-b">
                     <span><i class="fa-solid fa-magnifying-glass-plus orange-icon"></i></span> 
                     <span>
                         <p class="med-text">Accountability</p>
@@ -34,7 +38,7 @@
                     </span>
                 </div>
 
-                <div class="flex">
+                <div class="flex m-b">
                     <span><i class="fa-solid fa-folder-plus orange-icon"></i></span> 
                     <span>
                         <p class="med-text">Transparency</p>
@@ -42,7 +46,7 @@
                     </span>
                 </div>
 
-                <div class="flex">
+                <div class="flex m-b">
                     <span><i class="fa-solid fa-square-poll-vertical orange-icon"></i></span> 
                     <span>
                         <p class="med-text">Investment</p>
@@ -50,7 +54,42 @@
                     </span>
                 </div>
             </div>
+        </div>
 
+        <!--Terza parte (awards)-->
+        <div>
+            <p class="large-text text-center">Awards</p>
+
+            <div class="cards flex">
+                <div class="card-award">
+                    <img src="../assets/img/award-a-200x191.png" alt="seo" class="azure">
+                    <p>Winner Seo master MAGT Smart Start Award 2017</p>
+                </div>
+                <div class="card-award">
+                    <img src="../assets/img/award-b-200x191.png" alt="social" class="orange-img"> 
+                    <p>Top social Media Agencies Next Partner 2018</p>
+                </div>
+                <div class="card-award">
+                    <img src="../assets/img/award-c-200x191.png" alt="provider" class="pink">
+                    <p>10 Fastest Growing Abstract Solution Provider 2019</p>
+                </div>
+                <div class="card-award">
+                    <img src="../assets/img/award-d-200x191.png" alt="exellence" class="blue">
+                    <p>National Exellence Agencie Award Winner 2020</p>
+                </div>
+            </div>
+        </div>
+
+        <!--Quarta parte (team Avada)-->
+        <div class="bg-white flex">
+            <div class="big-box">
+                <img src="../assets/img/large-testimonial.jpg" alt="l-testimonial" class="large-cons">
+            </div>
+            <div class="big-box flex-justify-center padd-text">
+                <p class="large-text">"The team at Avada Marketing Consultant is fabulous. They helped us unlock our potential online and offline. We have experienced year on year growth due to their progressive approach." </p>
+                <p>Kate Schadler</p>
+                <p>Marketing Consultant - Abstract</p>
+            </div>
         </div>
 
     </section>
@@ -99,6 +138,35 @@ export default {
         padding: 15px;
         margin-left: 50px;
         margin-right: 15px;
+    }
+    .card-award {
+        width: 200px;
+        text-align: center;
+        margin: 3rem;
+
+        img {
+            width: 130px;
+        }
+        .azure {
+            filter: hue-rotate(170deg);
+        }
+        .orange-img {
+            filter: hue-rotate(10deg);
+        }
+        .pink {
+            filter: hue-rotate(340deg);
+        }
+        .blue {
+            filter: hue-rotate(200deg);
+        }
+    }
+}
+.bg-white {
+    background-color: white;
+
+    .large-cons {
+        width: 550px;
+        border-radius: 5px;
     }
 }
 </style>
